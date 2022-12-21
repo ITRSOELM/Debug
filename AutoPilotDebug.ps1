84f6a1e4eefa18baed9ed7abcb3d1dc41bbf64c1
Søren Elmholt <soelm@itrelation.dk>
2022-12-21 07:47:14 +0100

    Update AutoPilotDebug.ps1

--- AutoPilotDebug.ps1
+++ AutoPilotDebug.ps1
@@ -21,4 +21,12 @@ $Url = Get_TeamViewerQS  "https://get.teamviewer.com/a1s2d3"
 
 Invoke-WebRequest -Uri $Url -OutFile C:\Windows\Temp\TeamViewerQS.exe
 
-Start-Process -FilePath C:\Windows\Temp\TeamViewerQS.exe
+# prepare registry for startup:
+$reg = "HKCU:\\Software\TeamViewer"
+if (!(Test-Path $reg)) {New-Item -Path $reg -Force -ea 0}
+$null = New-ItemProperty -Path $reg -Name 'TeamViewerTermsOfUseAcceptedQS' -PropertyType 'DWord' -Value 1 -Force -ea 0
+$null = New-ItemProperty -Path $reg -Name 'EulaByUserAccepted_QS' -PropertyType 'DWord' -Value 1 -Force -ea 0
+$null = New-ItemProperty -Path $reg -Name 'IntroShown' -PropertyType 'DWord' -Value 1 -Force -ea 0
+
+
+Start-Process -FilePath C:\Windows\Temp\TeamViewerQS.exe 
